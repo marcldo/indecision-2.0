2c711e6dd6cc7136bef02f2a0e3615c2a7b5378e
gitMarcldo <marcldo@users.noreply.github.com>
2021-02-05 11:30:40 -0500

    initial file transfer

---
+++ src/playground/build-it-visible.js
@@ -0,0 +1,35 @@
+console.log("app is running")
+
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+    this.state = {
+      visibility: false
+    }
+  }
+  handleToggleVisibility() {
+    this.setState((prevState) => {
+      return {
+        visibility: !prevState.visibility
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.handleToggleVisibility}>{this.state.visibility ? 'Hide Details' : 'Show Details'}</button>
+        {this.state.visibility && <p>Details</p>}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
+
+
+
+
+
+
